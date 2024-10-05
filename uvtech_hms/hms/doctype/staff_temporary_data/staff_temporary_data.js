9400926a33c5ac9de6d13847b2fed8a52f14e0f3
uvtech-erpnext01 <uvtech.erpnext01@gmail.com>
2024-10-05 18:04:55 +0530

    feat: resize the uploaded images

---
+++ uvtech_hms/hms/doctype/staff_temporary_data/staff_temporary_data.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, gg and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Staff temporary data", {
+// 	refresh(frm) {
+
+// 	},
+// });
